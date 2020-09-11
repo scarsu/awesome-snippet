cc7c734ce242154059396091c9e93896d37f1fd0
scarsu <907866991@qq.com>
2020-09-11 03:41:24 +0000

    update

---
+++ vue-markStatic.js
@@ -0,0 +1,74 @@
+/**
+ * source code:https://github.com/vuejs/vue/blob/v2.6.12/src/compiler/optimizer.js
+ */
+export function optimize (root, options) {
+  if (!root) return
+  isStaticKey = genStaticKeysCached(options.staticKeys || '')
+  isPlatformReservedTag = options.isReservedTag || no
+  // 第一轮:标记所有非静态节点
+  markStatic(root)
+  // 第二步：标记静态根节点
+  markStaticRoots(root, false)
+}
+
+ function isStatic (node) {
+  if (node.type === 2) { // 表达式，返回 false
+    return false
+  }
+  if (node.type === 3) { // 静态文本，返回 true
+    return true
+  }
+  // 此处省略了部分条件
+  return !!(
+    !node.hasBindings && // 没有动态绑定
+    !node.if && !node.for && // 没有 v-if/v-for
+    !isBuiltInTag(node.tag) && // 不是内置组件 slot/component
+    !isDirectChildOfTemplateFor(node) && // 不在 template for 循环内
+    Object.keys(node).every(isStaticKey) // 非静态节点
+  )
+}
+
+function markStatic (node) {
+  node.static = isStatic(node)
+  if (node.type === 1) {
+    // 如果是元素节点，需要遍历所有子节点
+    for (let i = 0, l = node.children.length; i < l; i++) {
+      const child = node.children[i]
+      markStatic(child)
+      if (!child.static) {
+        // 如果有一个子节点不是静态节点，则该节点也必须是动态的
+        node.static = false
+      }
+    }
+  }
+}
+
+function markStaticRoots (node, isInFor) {
+  if (node.type === 1) {
+    if (node.static || node.once) {
+      node.staticInFor = isInFor
+    }
+    // For a node to qualify as a static root, it should have children that
+    // are not just static text. Otherwise the cost of hoisting out will
+    // outweigh the benefits and it's better off to just always render it fresh.
+    if (node.static && node.children.length && !(
+      node.children.length === 1 &&
+      node.children[0].type === 3
+    )) {
+      node.staticRoot = true
+      return
+    } else {
+      node.staticRoot = false
+    }
+    if (node.children) {
+      for (let i = 0, l = node.children.length; i < l; i++) {
+        markStaticRoots(node.children[i], isInFor || !!node.for)
+      }
+    }
+    if (node.ifConditions) {
+      for (let i = 1, l = node.ifConditions.length; i < l; i++) {
+        markStaticRoots(node.ifConditions[i].block, isInFor)
+      }
+    }
+  }
+}
